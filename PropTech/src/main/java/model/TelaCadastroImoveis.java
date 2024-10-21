e6cd2aad31636a00d90051b2e310673b7662ef55
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2024-10-21 16:21:34 -0300

    Comit tela de imóveis

--- PropTech/src/main/java/model/TelaCadastroImoveis.java
+++ PropTech/src/main/java/model/TelaCadastroImoveis.java
@@ -1,4 +1,35 @@
 package model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class TelaCadastroImoveis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipo;
+    private String cep;
+    private String numero;
+    private String endereco;
+    private String complemento;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+    public String getComplemento() { return complemento; }
+    public void setComplemento(String complemento) { this.complemento = complemento; }
 }
