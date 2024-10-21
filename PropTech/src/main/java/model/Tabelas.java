967c825720d9864f1515bba9370500728c80d15b
Gustavo Nethson <116599169+GustavoNethson@users.noreply.github.com>
2024-10-21 16:14:49 -0300

    Add files via upload

---
+++ PropTech/src/main/java/model/Tabelas.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Tabelas {
+
+    Integer nIdImov;
+    String stNomImov;
+    String stMesComp;
+    Double dValAlug;
+    Double dValGaragem;
+    Double dValCond;
+    Double dConsAgua;
+    Double dValAgua;
+    Double dConsGas;
+    Double dValGas;
+    Double dValLuz;
+    Double dValTotal;
+
+
+
+}
