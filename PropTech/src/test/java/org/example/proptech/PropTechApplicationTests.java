937a78fb0bdbce0fb096ad2e27ec5eea5f6e5816
Gustavo Nethson <116599169+GustavoNethson@users.noreply.github.com>
2024-10-21 14:31:59 -0300

    Versão 1.0

---
+++ PropTech/src/test/java/org/example/proptech/PropTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
