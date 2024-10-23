fb13262472c41346f68b6910d2575e3dd1cf2fdc
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2024-10-22 22:10:31 -0300

    Comit Cadastro Inquilinos Controller

--- PropTech/src/main/java/controller/CadastroInquilinosController.java
+++ PropTech/src/main/java/controller/CadastroInquilinosController.java
@@ -1,4 +1,44 @@
 package controller;
 
+import model.CadastroInquilinos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.CadastroInquilinosService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/inquilinos")
 public class CadastroInquilinosController {
+    @Autowired
+    private CadastroInquilinosService inquilinoService;
+
+    @GetMapping
+    public List<CadastroInquilinos> getAllInquilinos() {
+        return inquilinoService.getAllInquilinos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CadastroInquilinos> getInquilinoById(@PathVariable Long id) {
+        CadastroInquilinos inquilino = inquilinoService.getInquilinoById(id)
+                .orElseThrow(() -> new RuntimeException("Inquilino não encontrado"));
+        return ResponseEntity.ok(inquilino);
+    }
+
+    @PostMapping
+    public CadastroInquilinos createInquilino(@RequestBody CadastroInquilinos inquilino) {
+        return inquilinoService.createInquilino(inquilino);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CadastroInquilinos> updateInquilino(@PathVariable Long id, @RequestBody CadastroInquilinos inquilinoDetails) {
+        CadastroInquilinos updatedInquilino = inquilinoService.updateInquilino(id, inquilinoDetails);
+        return ResponseEntity.ok(updatedInquilino);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInquilino(@PathVariable Long id) {
+        inquilinoService.deleteInquilino(id);
+        return ResponseEntity.noContent().build();
+    }
 }
