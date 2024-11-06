c9665dcba7dbe67cf40ef78caa1519b74d5154d9
Gustavo Nethson <116599169+GustavoNethson@users.noreply.github.com>
2024-11-05 21:22:05 -0300

    Delete PropTech directory

--- PropTech/src/main/java/repository/CadastroImoveisRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.CadastroImoveis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CadastroImoveisRepository extends JpaRepository<CadastroImoveis, Long> {
-}
--- PropTech/src/main/java/repository/TabelasRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Tabelas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TabelasRepository extends JpaRepository<Tabelas, Integer> {
-}
